5e75867309750b08980e85ec3954cda049571246
AskhatErkenov <karahau0o@gmail.com>
2022-12-02 19:36:22 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Sublime project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+    <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+    <link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+    <header class="header">
+        <div class="header_container">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="header_content d-flex flex-row align-items-center justify-content-start">
+                            <div class="logo"><a href="#">Tech-store</a></div>
+                            <nav class="main_nav">
+                                <ul>
+                                    <li class="hassubs active">
+                                        <a href="index.php">Главная</a>
+                                    </li>
+                                    <li class="hassubs">
+                                        <a href="categories.php">Категории</a>
+                                    </li>
+                                    <li><a href="cart.php">Корзина</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+require('header.php');
+?>
+<div class="home">
+    <div class="home_slider_container">
+        <div class="owl-carousel owl-theme home_slider">
+            <div class="owl-item home_slider_item">
+                <div class="home_slider_background" style="background-image:url(images/product_1.jpg)"></div>
+            </div>
+            <div class="owl-item home_slider_item">
+                <div class="home_slider_background" style="background-image:url(images/product_2.jpg)"></div>
+            </div>
+            <div class="owl-item home_slider_item">
+                <div class="home_slider_background" style="background-image:url(images/product_3.jpg)"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="products">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="product_grid">
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_1.jpg" alt=""></div>
+                        <div class="product_extra product_new"><a href="categories.php">New</a></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_2.jpg" alt=""></div>
+                        <div class="product_extra product_sale"><a href="categories.php">Sale</a></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_3.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_4.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_5.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                    <!-- Product -->
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_6.jpg" alt=""></div>
+                        <div class="product_extra product_hot"><a href="categories.php">Hot</a></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                    <!-- Product -->
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_7.jpg" alt=""></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                    <!-- Product -->
+                    <div class="product">
+                        <div class="product_image"><img src="images/product_8.jpg" alt=""></div>
+                        <div class="product_extra product_sale"><a href="categories.php">Hot</a></div>
+                        <div class="product_content">
+                            <div class="product_title"><a href="product.php">Smart Phone</a></div>
+                            <div class="product_price">$670</div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require('footer.php');
+?>
